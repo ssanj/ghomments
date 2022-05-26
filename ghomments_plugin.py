b2a9025bb4cd9ab585a5284dbd642545591404f5
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-05-26 20:44:45 +1000

    Support removing comments

--- ghomments_plugin.py
+++ ghomments_plugin.py
@@ -6,6 +6,60 @@ import json
 import html
 
 
+class GhommentsPreloaderCommand(sublime_plugin.EventListener):
+
+    DEFAULT_PURS_HOME= os.path.expanduser("~/.purs")
+
+    def is_purs_checkout(self, file_name):
+        # Override this from config
+        common = os.path.commonprefix([self.DEFAULT_PURS_HOME, file_name])
+        return common == self.DEFAULT_PURS_HOME
+
+    def get_window_ghomment_config(self, window):
+        return window.settings().get("ghomment")
+
+    def set_window_ghomment_config(self, window):
+        window.settings().set("ghomment", True)
+        print("set and get: {}".format(window.settings().get("ghomment")))
+
+
+    def unset_window_ghomment_config(self, window):
+        window.settings().set("ghomment", False)
+
+    def on_load_async(self, view):
+        window = view.window()
+        if self.get_window_ghomment_config(window) is None: #config not set
+            if self.is_purs_checkout(view.file_name()): # valid config
+                self.set_window_ghomment_config(window)
+                print("Ghomments: active")
+            else:
+                self.unset_window_ghomment_config(window)
+                print("Ghomments: inactive")
+
+        if self.get_window_ghomment_config(window):
+            print("Ghomments: loading comments")
+            if self.has_comments(view):
+                view.run_command('ghomments')
+        else:
+            print("Ghomments: not loading comments")
+
+    def has_comments(self, view):
+        if view.file_name():
+            full_file_name = view.file_name()
+            file_dir = os.path.dirname(full_file_name)
+            file_name = os.path.basename(full_file_name)
+            comment_file_name = "{}.comment".format(file_name)
+
+            comment_file = os.sep.join([file_dir, comment_file_name])
+            print("Ghomments: file_dir: {}".format(file_dir))
+            print("Ghomments: file_name: {}".format(file_name))
+            print("Ghomments: comment_file_name: {}".format(comment_file_name))
+            print("Ghomments: final comment file: {}".format(comment_file))
+            has_comments = os.path.exists(comment_file)
+            print("Ghomments: has comment file: {}".format(has_comments))
+            return has_comments
+
+
 class GhommentsCommand(sublime_plugin.TextCommand):
 
     comment_regions = []
@@ -25,6 +79,12 @@ class GhommentsCommand(sublime_plugin.TextCommand):
         logging.basicConfig(format=FORMAT)
         self.logger = logging.getLogger('ghomments.plugin')
         self.logger.debug("regions: {}".format(str(self.comment_regions)))
+
+        # remove any existing phantoms
+        if args.get('reset'):
+            print("resetting all the things")
+            self.reset()
+            return
 
         if args.get('index') and len(self.comment_regions) != 0:
             self.logger.debug("has index supplied")
@@ -46,6 +106,14 @@ class GhommentsCommand(sublime_plugin.TextCommand):
 
     # def is_visible(self):
     #     return scoggle.Scoggle.is_visible(self.view, sublime.version())
+
+    def reset(self):
+        self.comment_index = 0
+        self.comment_regions = []
+
+        self.ps = sublime.PhantomSet(self.view, "GHComments")
+        self.phantoms = []
+        self.ps.update(self.phantoms)
 
     def perform(self):
         self.logger.setLevel(logging.DEBUG);
